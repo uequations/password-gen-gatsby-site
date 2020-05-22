c7e3c9e7e487d178c058b0e60d027a97a2844f08
malkebu-lan <malkebu-lan@uequations.com>
2020-05-22 16:11:35 -0400

    implementing page loader

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,23 @@
-import React, { Suspense } from "react"
+import React, { useEffect, useState } from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Hero from "../components/Hero"
 import GoodPasswordInfo from "../components/GoodPasswordInfo"
+import Skeleton from "@material-ui/lab/Skeleton"
 
+export default function IndexPage() {
 
-const IndexPage = () => (
-  <Suspense fallback={<p>Loading...</p>}>
+  const [isLoading, setIsLoading] = useState({ isLoading: true })
+
+  useEffect(() => {
+    setIsLoading(false)
+  })
+
+  return (isLoading ? (<Skeleton variant="rect"/>) : (
     <Layout>
       <SEO title="Password Generator | Universal Equations"/>
       <Hero/>
       <GoodPasswordInfo/>
-    </Layout>
-  </Suspense>
-
-)
-
-export default IndexPage
+    </Layout>))
+}
