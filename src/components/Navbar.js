a663692d7d98cdb60a31565c548947534fb8536d
malkebu-lan <malkebu-lan@uequations.com>
2020-05-22 19:24:38 -0400

    corrected nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,48 @@
 import React from "react"
+import AppBar from "@material-ui/core/AppBar"
+import siteTheme from "../theme"
+import { ThemeProvider } from "@material-ui/core/styles"
+import Avatar from "@material-ui/core/Avatar"
+import Toolbar from "@material-ui/core/Toolbar"
+import Button from "@material-ui/core/Button"
+import Typography from "@material-ui/core/Typography"
+import makeStyles from "@material-ui/core/styles/makeStyles"
+import IconButton from "@material-ui/core/IconButton"
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+}))
 
 const Navbar = () => {
+  const classes = useStyles()
+
   return (
-    <>
-      <header>
-        <a href="https://www.uequations.com">
-          <h4 className="logo">UNIVERSAL EQUATIONS</h4>
-        </a>
-        <nav>
-          <ul>
-            <li><a href="https://www.uequations.com">HOME</a></li>
-            <li><a href="https://www.uequations.com/about-us">ABOUT</a></li>
-            <li><a href="https://sales.uequations.com/contact-us-general-inquiry/">CONTACT</a></li>
-          </ul>
-        </nav>
-      </header>
-    </>
+    <ThemeProvider theme={siteTheme}>
+      <div className={classes.root}>
+        <AppBar position={"static"} color={"primary"}>
+          <Toolbar>
+            <IconButton className={classes.menuButton} href="https://www.uequations.com">
+              <Avatar
+                src={"https://res.cloudinary.com/uequations/image/upload/v1590188520/password-gen-gatsby-site/ueq_ps_crop_circle_logo_100px.png"}/>
+            </IconButton>
+            <Typography className={classes.title} variant={"h5"}>
+              UNIVERSAL EQUATIONS
+            </Typography>
+            <Button color={"secondary"} href="https://www.uequations.com">HOME</Button>
+            <Button color={"secondary"} href="https://www.uequations.com/about-us">ABOUT</Button>
+            <Button color={"secondary"} href="https://sales.uequations.com/contact-us-general-inquiry/">CONTACT</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+    </ThemeProvider>
   )
 }
 
